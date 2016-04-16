6e2f6d8dca9f139072c618caafb09fb8f43bc701
Ming Hsieh <xiemingzhi@gmail.com>
2016-04-16 10:11:10 +0800

    DAO Get rid of spring dependencies Using session to do CRUD

--- src/main/java/com/mycompany/dao/ContactDAOHibernate.java
+++ src/main/java/com/mycompany/dao/ContactDAOHibernate.java
@@ -8,54 +8,45 @@ import org.apache.commons.logging.LogFactory;
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
-
-import org.springframework.orm.hibernate3.HibernateCallback;
-import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.hibernate.SessionFactory;
 
 import com.mycompany.Contact;
 
-
-public class ContactDAOHibernate extends HibernateDaoSupport implements ContactDAO{
+public class ContactDAOHibernate implements ContactDAO{
 	protected final Log logger = LogFactory.getLog(getClass());
+
+	SessionFactory sessionFactory;
+	
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
 	
 	public void insertContact(Contact contact) {
 		logger.debug("Inside ContactDAOHibernate.insert() method");
-		this.getHibernateTemplate().save(contact);
+		sessionFactory.getCurrentSession().save(contact);
 	}
 	
 	public List getContacts() {
 		logger.debug("Inside ContactDAOHibernate.getContacts() method");
-		return getHibernateTemplate().executeFind(new HibernateCallback() {
-			public Object doInHibernate(Session session)
-				throws HibernateException, SQLException {
-
-				StringBuffer sb = new StringBuffer(100);
-				sb.append("select distinct contact ");
-				sb.append("from Contact contact ");
-				sb.append("order by contact.contactId");
-
-				Query query = session.createQuery(sb.toString());
-				List list = query.list();
-
-				return list;
-			}
-		});		
+		Query query = sessionFactory.getCurrentSession().createQuery("select distinct contact from Contact contact order by contact.contactId");
+		List list = query.list();
+		return list;
 	}
 	
 	public Contact getContact(int contactId) {
 		logger.debug("Inside ContactDAOHibernate.getContact() method");
-		return (Contact)getHibernateTemplate().get(Contact.class, contactId);
+		return (Contact)sessionFactory.getCurrentSession().get(Contact.class, contactId);
 	}
 	
 	public void updateContact(Contact contact) {
 		logger.debug("Inside ContactDAOHibernate.updateContact() method");
-		this.getHibernateTemplate().update(contact);
+		this.sessionFactory.getCurrentSession().update(contact);
 	}
 	
 	public void deleteContact(int contactId) {
 		logger.debug("Inside ContactDAOHibernate.deleteContact() method");
-		Contact c = (Contact)getHibernateTemplate().get(Contact.class, contactId);
-		if (c!=null) this.getHibernateTemplate().delete(c);
+		Contact c = (Contact)sessionFactory.getCurrentSession().get(Contact.class, contactId);
+		if (c!=null) this.sessionFactory.getCurrentSession().delete(c);
 		else logger.debug("Inside ContactDAOHibernate.deleteContact() method no such id found");
 	}
 	
